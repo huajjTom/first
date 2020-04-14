90492e0a1289348090ad896c609a7c06a1fa0672
huajjTom <hncshjj@163.com>
2020-04-14 13:20:01 +0800

    hello--本地修改后提交

--- firstHelloWorld/src/com/company/Main.java
+++ firstHelloWorld/src/com/company/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
+        System.out.println("Hi World first!!!!");
     }
 }
